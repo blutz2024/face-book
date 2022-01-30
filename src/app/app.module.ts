b4df02a365a0162e6b6fa59e1017e1570bb0b595
Bella <idl00001@mix.wvu.edu>
2022-01-30 16:31:43 -0500

    Refactoring: Moved Nav direct inside App directory

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { TopNavBarComponent } from 'src/Navigation/topnavbar.component';
+import { TopNavBarComponent } from 'src/app/Navigation/topnavbar.component';
 
 import { AppComponent } from './app.component';
 
